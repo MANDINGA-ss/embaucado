c30999e091932fe6f0db9bfdedd3574879ab5222
Nicole <109987735+Nicole057@users.noreply.github.com>
2024-06-14 13:45:33 -0300

    Update funciones.h

--- funciones.h
+++ funciones.h
@@ -4,22 +4,28 @@
 #include <iostream>
 using namespace std;
 
+/// Menu
 void menu();
-int jugar();
-void creditos();
+void jugar(string &primerJugador, string &segundoJugador); /// #1
+                    /// #2
+void creditos();    /// #3
 
+/// Maso
 int generarNroAleatorio();
 void generarMano(int v[], int tam);
 void mostrarMano(int v[], int tam);
 
-void obtenerCarta(int id, int &valor, string &nombre, string &palo);
+void obtenerCarta(int id, int &valor, string &carta, string &figura);
 
-void cartaEmbaucadora (int v[], int tam);
+/// Embaucadora
+void embaucadora (int v[], int tam, string &figura);
 
+/// Organizacion de masos
 int contarNumeros(int v[], int tam, int numero);
 bool verificarMano(int v[], int tam);
 void ordenarMano(int v[], int tam);
 
-void primeraRonda(int v[], int tam);
+/// Rondas
+void primeraRonda(string primerJugador, string segundoJugador);
 
 #endif // FUNCIONES_H_INCLUDED
